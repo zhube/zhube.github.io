c1ac73f4a230f25f7f07df8a65a1710138cd34c8
zhube <zaneperkone@yahoo.com>
2018-02-22 12:07:15 +0200

    Initial commit

--- 01compass_empty.js
+++ 01compass_empty.js
@@ -7,8 +7,25 @@ oriented = function(e) {
         }   else  { 
             compassHeading = e.alpha;
         }
+
+        if(compassHeading > 315 || compassHeading < 45) {
+                text_area.textContent = "light";
+        }
+
+        if(compassHeading >= 45 && compassHeading < 135) {
+                text_area.textContent = "dawn";
+        }
+
+        if(compassHeading >= 135 && compassHeading < 225) {
+                text_area.textContent = "earth";
+        }
+
+        if(compassHeading >= 225 && compassHeading < 315) {
+                text_area.textContent = "new beggining";
+        }
+
         
-         text_area.textContent = compassHeading;
+         //text_area.textContent = compassHeading;
         //calls function to alter content based on heading
        // myOrientation();
     }
